756bd9ec010bfc6a65240ea0d32202a080745ec3
eidyz <eidyz819@gmail.com>
2020-01-01 17:29:20 +0100

    :sparkles: switch to memorygo

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/JamesHovious/w32 v1.1.0
-	github.com/eidyz/memorygo v0.0.0-20191128223842-0b1ba1b60610
+	github.com/eidyz/memorygo v0.0.1
 	golang.org/x/sys v0.0.0-20191228213918-04cbcbbfeed8
 )
--- main.go
+++ main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/JamesHovious/w32"
-	"golang.org/x/sys/windows"
 	"os"
-	"syscall"
-	"unsafe"
+
+	"github.com/JamesHovious/w32"
+	memory "github.com/eidyz/memorygo"
+	"golang.org/x/sys/windows"
 )
 
 var modkernel32 = windows.NewLazySystemDLL("kernel32.dll")
+var hKernel32 = modkernel32.Handle()
 var getModuleHandleA = modkernel32.NewProc("GetModuleHandleA")
 var loadLibraryA = modkernel32.NewProc("LoadLibraryA")
 
@@ -20,65 +21,9 @@ func input(q string) string {
 	return s
 }
 
-func GetModuleHandleA(moduleName string) w32.HANDLE {
-	s16, err := syscall.UTF16PtrFromString(moduleName)
-	if err != nil {
-		panic("err")
-	}
-	r1, r2, e1 := syscall.Syscall(getModuleHandleA.Addr(), 1, uintptr(unsafe.Pointer(s16)), 0, 0)
-	fmt.Println(r1, r2, e1)
-	if r1 == 0 {
-		if e1 != 0 {
-			fmt.Println(syscall.Errno(e1))
-
-		} else {
-			err = syscall.EINVAL
-		}
-	}
-	return w32.HANDLE(r1)
-}
-
-// GetProcessName ---
-func GetProcessName(id uint32) string {
-	snapshot := w32.CreateToolhelp32Snapshot(w32.TH32CS_SNAPMODULE, id)
-	if snapshot == w32.ERROR_INVALID_HANDLE {
-		return "<UNKNOWN>"
-	}
-	defer w32.CloseHandle(snapshot)
-
-	var me w32.MODULEENTRY32
-	me.Size = uint32(unsafe.Sizeof(me))
-	if w32.Module32First(snapshot, &me) {
-		return w32.UTF16PtrToString(&me.SzModule[0])
-	}
-
-	return "<UNKNOWN>"
-}
-
-// ListProcesses ---
-func ListProcesses() []uint32 {
-	sz := uint32(1000)
-	procs := make([]uint32, sz)
-	var bytesReturned uint32
-	if w32.EnumProcesses(procs, sz, &bytesReturned) {
-		return procs[:int(bytesReturned)/4]
-	}
-	return []uint32{}
-}
-
-// TODO: SWITCH TO MEMORYGO
-// FindProcessByName ---
-func FindProcessByName(name string) (uint32, error) {
-	for _, pid := range ListProcesses() {
-		if GetProcessName(pid) == name {
-			return pid, nil
-		}
-	}
-	return 0, fmt.Errorf("unknown process")
-}
-
 func main() {
-	pid, err := FindProcessByName("Hyper.exe")
+	fmt.Println("kernel", modkernel32, hKernel32)
+	pid, err := memory.FindProcessByName("notepad.exe")
 	if err != nil {
 		panic(err)
 	}
@@ -103,10 +48,15 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("Arg adress:", argAddress)
-
 	bytesW := w32.WriteProcessMemory(h, uint32(argAddress), []byte(dll), uint(len(dll)))
 	fmt.Println("Bytes written:", bytesW)
-	_, threadId, err := w32.CreateRemoteThread(h, nil, 0, uint32(loadLibraryA.Addr()), argAddress, 0)
+
+	loadLib, err := w32.GetProcAddress(w32.HANDLE(hKernel32), "LoadLibraryA")
+	if err != nil {
+		fmt.Println("GetProcAddress failed")
+		panic(err)
+	}
+	_, threadId, err := w32.CreateRemoteThread(h, nil, 0, uint32(loadLib), argAddress, 0)
 	if err != nil {
 		fmt.Println("CreateRemoteThread failed")
 		panic(err)
